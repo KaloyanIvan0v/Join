bad4e023ddb07a99ddbcfd9773844a54f82e4ce0
Kaloyan Ivanov <kaloyanivanov@IvaBook.local>
2024-04-04 22:36:18 +0200

    fixed bug on register

--- js/register.js
+++ js/register.js
@@ -1,18 +1,20 @@
 let users = [];
-
+let a = ["a", "b", "c", "d", "e", "k"];
 async function initRegister() {
-  loadUsers();
+  await loadUsers();
 }
 
 async function loadUsers() {
   try {
     users = JSON.parse(await getItem("users"));
+    console.log("Type of users:", typeof users);
   } catch (e) {
     console.error("Loading error:", e);
   }
 }
 
 async function register() {
+  const registerBtn = document.getElementById("registerBtn");
   registerBtn.disabled = true;
   users.push({
     name: names.value,
@@ -23,17 +25,11 @@ async function register() {
   resetForm();
 }
 
-function getInputValues() {
-  let name = document.getElementById("name").value;
-  let email = document.getElementById("email").value;
-  let password = document.getElementById("password").value;
-  let passwordConfirm = document.getElementById("password-confirm").value;
-  let inputData = [name, email, password, passwordConfirm];
-  return inputData;
-}
-
 function resetForm() {
+  const registerBtn = document.getElementById("registerBtn");
+  names.value = "";
   email.value = "";
   password.value = "";
-  registerBtn.disabled = false;
+  document.getElementById("password-confirm").value = "";
+  registerBtn.disabled = true;
 }
