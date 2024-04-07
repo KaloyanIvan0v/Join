f6029ac45021d91f34fd58c6fe22b84e6441831f
Kaloyan Ivanov <kaloyanivanov@IvaBook.local>
2024-04-07 13:35:41 +0200

    edit sign up

--- js/register.js
+++ js/register.js
@@ -2,16 +2,44 @@ let users = [];
 let checkBoxState = false;
 async function initRegister() {
   await loadUsers();
+  setPwdInputEventListeners();
 }
 
 async function register() {
   let inputEmail = email.value;
-  if (userExist(inputEmail)) {
-    console.log("user exists", inputEmail);
-  } else {
+  if (!userExist(inputEmail) && passwordMatch()) {
     console.log("user will be created", inputEmail);
     registerNewUser();
+  } else {
+    handleLoginFeedbackMsg(inputEmail);
   }
+}
+
+function handleLoginFeedbackMsg(inputEmail) {
+  if (userExist(inputEmail)) {
+    SetLoginFeedbackMsg("User already exists!", 3000);
+  } else if (!passwordMatch()) {
+    SetLoginFeedbackMsg("Ups! your password don’t match", 3000);
+  }
+}
+
+function SetLoginFeedbackMsg(errMsg, duration) {
+  let feedbackField = document.getElementById("id-input-feedback");
+  feedbackField.innerHTML = setLoginFeedbackMsgHtml(errMsg);
+
+  setTimeout(() => {
+    removeFeddbackMsg(feedbackField);
+  }, duration);
+}
+
+function setLoginFeedbackMsgHtml(errMsg) {
+  return /*html*/ `
+${errMsg}
+`;
+}
+
+function removeFeddbackMsg(divId) {
+  divId.innerHTML = "";
 }
 
 async function registerNewUser() {
@@ -46,7 +74,9 @@ function userExist(user) {
   return false;
 }
 
-function passwordMatch(password, passwordConfirm) {
+function passwordMatch() {
+  let password = document.getElementById("password").value;
+  let passwordConfirm = document.getElementById("password-confirm").value;
   if (password == passwordConfirm) {
     return true;
   } else {
@@ -62,5 +92,27 @@ function toggleCheckbox() {
   } else {
     checkBoxState = false;
     checkBox.src = "../img/box-unchecked.png";
+  }
+}
+
+function setPwdInputEventListeners() {
+  document.addEventListener("click", function (event) {
+    inputClicked(event.target.id);
+  });
+}
+
+function inputClicked(id) {
+  if (id == "password" || id == "password-confirm") {
+    document.getElementById(id + "-img").src = "../img/visibility_off.png";
+  } else {
+    document.getElementById("password-confirm-img").src = "../img/lock.svg";
+    document.getElementById("password-img").src = "../img/lock.svg";
+  }
+}
+
+function togglePswVisibility(id) {
+  let img = document.getElementById(id);
+  console.log(img.src);
+  if (img.src == "../img/lock.svg") {
   }
 }
