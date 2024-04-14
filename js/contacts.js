29774707b766a579318849bed83eaeeefe6751c4
Kaloyan Ivanov <kaloyanivanov@IvaBook.local>
2024-04-14 10:22:06 +0200

    add func: getContacts

--- js/contacts.js
+++ js/contacts.js
@@ -1,22 +1,5 @@
 let contacts;
 
-const contactColor = {
-  1: "rgb(255, 187, 44)",
-  2: "rgb(255, 70, 70)",
-  3: "rgb(255, 230, 44)",
-  4: "rgb(195, 255, 43)",
-  5: "rgb(0, 56, 255)",
-  6: "rgb(255, 199, 3)",
-  7: "rgb(252, 113, 255)",
-  8: "rgb(255, 163, 94)",
-  9: "rgb(32, 215, 194)",
-  10: "rgb(6, 190, 232)",
-  11: "rgb(147, 39, 255)",
-  12: "rgb(110, 82, 255)",
-  13: "rgb(255, 94, 179)",
-  14: "rgb(255, 122, 1)",
-};
-
 function initContacts() {
   includeHTML();
   getContacts();
@@ -75,14 +58,15 @@ function handleHoverCancleButton() {
 }
 
 function printContactsByLetter(contacts) {
+  const contactList = document.getElementById("id-contacts-list");
   const sortedContacts = contacts.sort((a, b) => a.name.localeCompare(b.name));
   let currentLetter = null;
-
+  contactList.innerHTML = "";
   sortedContacts.forEach((contact) => {
     const firstLetter = contact.name.charAt(0).toUpperCase();
 
     if (firstLetter !== currentLetter) {
-      console.log(`Letter ${firstLetter}`);
+      contactList.innerHTML += renderLetterSectionHTML(firstLetter);
       currentLetter = firstLetter;
     }
 
@@ -91,5 +75,8 @@ function printContactsByLetter(contacts) {
 }
 
 function renderLetterSectionHTML(firstLetter) {
-  return;
+  return /*html*/ `
+  <div class="contact-list-first-letter">${firstLetter}</div>
+  <div class="contact-list-letter-parting-line"></div>
+  `;
 }
