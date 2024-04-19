864d624fb5c562adab268be14dc5d44f71f0837b
MKiessling88 <163688688+MKiessling88@users.noreply.github.com>
2024-04-19 17:01:30 +0200

    edit summery.html

--- js/summery.js
+++ js/summery.js
@@ -1,4 +1,25 @@
+let tasks;
+
 function init() {
     includeHTML();
-    setTimeout(loadFirstLettersFromSessionStorage, 10);
+    setTimeout(loadFirstLettersFromSessionStorage, 20);
+    loadTasks();
+    writeNumberOfAllTasks();
+}
+
+function loadTasks(){
+    tasks = JSON.parse(sessionStorage.getItem('tasks'));
+}
+
+function writeNumberOfAllTasks(){
+    document.getElementById('numberOfTasksInBoard').innerHTML = tasks.length;
+
+}
+
+function filterHighestPrio(){
+    let highestPrio = "Urgent";
+    let count = tasks.reduce(function(acc, tasks) {
+        return tasks.prio === highestPrio ? acc + 1 : acc;
+    }, 0);
+    document.getElementById('highestPrio').innerHTML = count;
 }
