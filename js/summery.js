c8b92c5ea0a9112ae304e092f3dccdc9c9b0af7f
MKiessling88 <163688688+MKiessling88@users.noreply.github.com>
2024-04-20 08:32:05 +0200

    edit summery.html

--- js/summery.js
+++ js/summery.js
@@ -5,20 +5,35 @@ function init() {
     setTimeout(loadFirstLettersFromSessionStorage, 20);
     loadTasks();
     writeNumberOfAllTasks();
+    filterHighestPrio();
+    countStatements('toDo');
+    countStatements('done');
+    countStatements('inProgress');
+    countStatements('awaitFeedback');
 }
 
-function loadTasks(){
+function loadTasks() {
     tasks = JSON.parse(sessionStorage.getItem('tasks'));
 }
 
-function writeNumberOfAllTasks(){
+function writeNumberOfAllTasks() {
     document.getElementById('numberOfTasksInBoard').innerHTML = tasks.length;
 }
 
-function filterHighestPrio(){
+function filterHighestPrio() {
     let highestPrio = "Urgent";
-    let count = tasks.reduce(function(acc, tasks) {
+    let count = tasks.reduce(function (acc, tasks) {
         return tasks.prio === highestPrio ? acc + 1 : acc;
     }, 0);
     document.getElementById('highestPrio').innerHTML = count;
+}
+
+function countStatements(index) {
+    let statementCounts = 0;
+    tasks.forEach(task => {
+        if (task.statement === index) {
+            statementCounts++;
+        }
+    });
+    document.getElementById(`count${index}`).innerHTML = statementCounts;
 }
